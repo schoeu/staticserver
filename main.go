c6b1730122cb48a00858f33e4581b718ebf74b02
schoeu89 <schoeu89@gmail.com>
2018-10-27 22:53:41 +0800

    delete prefix part.

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-    "fmt"
 	"flag"
+	"fmt"
 	"net/http"
-	"strings"
 )
-
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "I'm ready for that, you know.")
-}
 
 func main() {
 	var prefix, filePath, port, help string
 	flag.StringVar(&filePath, "path", "", "Path to files direction.")
-	flag.StringVar(&prefix, "prefix", "/static/", "Prefix of url path.")
+	flag.StringVar(&prefix, "prefix", "", "Prefix of url path.")
 	flag.StringVar(&port, "port", "8910", "Static server port.")
 	flag.StringVar(&help, "help", "", "help info for staticserver")
 	flag.Parse()
@@ -24,21 +19,11 @@ func main() {
 		return
 	}
 
-	// index page.
-    http.HandleFunc("/", index)
-
 	fsh := http.FileServer(http.Dir(filePath))
-	prefix = pathProcess(prefix)
-    http.Handle(prefix, http.StripPrefix(prefix, fsh))
+	http.Handle("/", fsh)
 
-    err := http.ListenAndServe(":" + port, nil)
-    if err != nil {
-        fmt.Println("ListenAndServe: ", err)
-    }
-}
-
-// trans prefix
-func pathProcess(p string) string{
-	purePrefix := strings.Replace(p, "/", "", -1)
-	return "/" + purePrefix + "/"
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		fmt.Println("ListenAndServe: ", err)
+	}
 }
