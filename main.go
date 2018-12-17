07febce6f59e239fcb12b60b3ba96e316784ff8b
schoeu89 <schoeu89@gmail.com>
2018-12-17 20:20:00 +0800

    delete prefix part.

--- main.go
+++ main.go
@@ -7,9 +7,8 @@ import (
 )
 
 func main() {
-	var prefix, filePath, port, help string
+	var filePath, port, help string
 	flag.StringVar(&filePath, "path", "", "Path to files direction.")
-	flag.StringVar(&prefix, "prefix", "", "Prefix of url path.")
 	flag.StringVar(&port, "port", "8910", "Static server port.")
 	flag.StringVar(&help, "help", "", "help info for staticserver")
 	flag.Parse()
@@ -18,6 +17,8 @@ func main() {
 		fmt.Println("Need path of files direction.")
 		return
 	}
+
+	fmt.Printf("Server on http://127.0.0.1:%s\n", port)
 
 	fsh := http.FileServer(http.Dir(filePath))
 	http.Handle("/", fsh)
